1211f70077e784a9fd302f6e685938e2138707e1
AndreaGonzato <gonzatoandrea98@gmail.com>
2020-04-26 13:12:57 +0200

    final version

--- src/esercizio-C-2020-04-24.c
+++ src/esercizio-C-2020-04-24.c
@@ -11,7 +11,6 @@ int main(){
 	char * text_to_write = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Non enim praesent elementum facilisis leo vel fringilla est ullamcorper. Non quam lacus suspendisse faucibus interdum. Metus vulputate eu scelerisque felis imperdiet. At ultrices mi tempus imperdiet nulla malesuada pellentesque. At tempor commodo ullamcorper a lacus vestibulum. Consequat semper viverra nam libero justo laoreet sit amet. Facilisis magna etiam tempor orci eu. Convallis tellus id interdum velit laoreet id donec ultrices. Interdum velit euismod in pellentesque massa placerat duis ultricies. Senectus et netus et malesuada fames. Pharetra vel turpis nunc eget lorem dolor. Nisi porta lorem mollis aliquam ut porttitor leo a. Euismod elementum nisi quis eleifend quam adipiscing vitae proin.";
 	int text_to_write_len = strlen(text_to_write);
 
-
 	// open a file for writing
 	int fd = open("output.txt",
 				  O_CREAT | O_TRUNC | O_WRONLY,
@@ -37,7 +36,6 @@ int main(){
 		case 0:
 		{
 			//child
-			printf("child\n");
 			// SEEK_SET: posizione offset a (text_to_write_len/2) rispetto all'inizio del file
 			off_t file_offset = lseek(fd, text_to_write_len/2 , SEEK_SET);
 			if(file_offset == -1){
@@ -67,8 +65,7 @@ int main(){
 		default:
 		{
 			// father
-			sleep(1);
-			printf("father\n");
+
 			off_t file_offset = lseek(fd, 0, SEEK_SET);
 			if(file_offset == -1){
 				perror("lseek()");
@@ -87,7 +84,6 @@ int main(){
 			}
 		}
 	}
-
 
 	// close file
 	if (close(fd) == -1) {
